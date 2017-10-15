df066cef18c7c749abccdac2fd7af91992958a6f
Allen B <pingguo.ny+git@gmail.com>
2017-10-15 07:51:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ Winter = {};
 Winter.CreateWindow = function(title) {
   var win = document.createElement("div");
   win.classList.add("winter-window");
+  var titlebar = document.createElement("div");
+  titlebar.classList.add("winter-window-title");
+  win.appendChild(titlebar);
+  document.body.appendChild(win);
 }
 Winter.App = function(name) {
   this.name = name;
