cf4b8de37a9e2ef9fe6b817ea3d0fb719efcc50a
ALlen B <pingguo.ny@gmail.com>
2017-10-15 20:11:11 -0400

    stuff

--- script.js
+++ script.js
@@ -16,12 +16,20 @@ Winter.CreateWindow = function(title) {
     win.appendChild(titlebar);
     document.body.appendChild(win);
 
-    win.onmousemove = function(e) {
+    function move(x, y) {
+        win.style.top = y - 24 + "px";
+        win.style.left = x - 32 + "px";
+    }
+
+    titlebar.onmousemove = function(e) {
         if(Winter.mousedown == 1) {
-            win.style.top = e.clientY - 24 + "px";
-            win.style.left = e.clientX - 32 + "px";
+            move(e.clientX, e.clientY);
         }
     }
+
+    titlebar.addEventListener("touchmove", function(e) {
+        move(e.pageX, e.pageY);
+    });
     return win;
 }
 Winter.App = function(name, about) {
@@ -50,25 +58,50 @@ Winter.WebApp = function(name, about, url) {
 }
 Winter.WebApp.prototype = Object.create(Winter.App.prototype);
 Winter.WebApp.prototype.setWidth = function(w) {
-    Winter.App.setWidth.call(this, w);
+    Winter.App.prototype.setWidth.call(this, w);
     this.frame.width = w;
 }
 Winter.WebApp.prototype.setHeight = function(h) {
-    Winter.App.setHeight.call(this, h);
+    Winter.App.prototype.setHeight.call(this, h);
     this.frame.width = h;
 }
 
 Winter.Apps = {};
 Winter.Apps.Browser = function(url) {
     this.app = new Winter.App("Browser", "Browse the web");
+    this.address = document.createElement("input");
+    this.address.value = "https://start.duckduckgo.com/";
+    this.address.style.display = "block";
+    this.address.style.width = "100%";
+    this.address.style.padding = "8px";
+    this.address.style.boxSizing = "border-box";
+    var self = this;
+    this.address.onkeypress = function(e) {
+        if(e.keyCode == 13)
+            self.goto(this.value);
+    }
+    this.address.style.border = 0;
+    this.app.content.appendChild(this.address);
     this.frame = document.createElement("iframe");
     this.frame.style.border = 0;
     this.app.content.appendChild(this.frame);
     this.frame.src = "https://start.duckduckgo.com/";
-    this.frame.width = "400px";
+    this.frame.width = "500px";
     this.frame.height = "500px";
-    this.app.setWidth(400);
+    this.app.setWidth(500);
     this.app.setHeight(500);
+}
+
+Winter.Apps.Browser.prototype.goto = function(query) {
+    if(query.indexOf("http") == 0)
+        this.frame.src = query;
+    else
+        this.frame.src = "https://duckduckgo.com/?q=" + query;
+}
+
+Winter.Apps[2048] = function() {
+    this.app = new Winter.WebApp("2048", "1024 clone", "https://gabrielecirulli.github.io/2048/");
+    this.app.setWidth(300);
 }
 
 window.addEventListener("load", function() {
