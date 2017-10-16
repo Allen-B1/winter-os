54e9b8fd2d4bff08cb4cef5ae4c3959fdaef3c6c
ALlen B <pingguo.ny@gmail.com>
2017-10-16 19:26:53 -0400

    jQuery

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 Winter = {};
 Winter.mousedown = 0;
 Winter.CreateWindow = function(title) {
-    var win = document.createElement("div");
-    win.classList.add("winter-window");
-    var titlebar = document.createElement("div");
-    titlebar.classList.add("winter-window-title");
-    titlebar.innerHTML = title;
-    var closebtn = document.createElement("div");
-    closebtn.classList.add("winter-window-title-close");
-    closebtn.innerHTML = "&times;";
-    titlebar.appendChild(closebtn);
-    closebtn.onclick = function() {
-        win.remove();
-    }
-    win.appendChild(titlebar);
-    document.body.appendChild(win);
+    var win = $("<div/>");
+    win.addClass("winter-window");
+    var titlebar = $("<div/>");
+    titlebar.addClass("winter-window-title");
+    titlebar.html(title);
+    var closebtn = $("<div/>");
+    closebtn.addClass("winter-window-title-close");
+    closebtn.html("&times;");
+    closebtn.appendTo(titlebar);
+    closebtn.click(function() {
+        setTimeout(function() {win.remove();}, 100);
+    });
+    titlebar.appendTo(win);
+    win.appendTo($(document.body));
 
     function move(x, y) {
-        win.style.top = y - 24 + "px";
-        win.style.left = x - 32 + "px";
+        win.css("top", y - 24 + "px");
+        win.css("left",x - 32 + "px");
     }
 
-    titlebar.onmousemove = function(e) {
+    titlebar.mousemove(function(e) {
         if(Winter.mousedown == 1) {
             move(e.clientX, e.clientY);
         }
-    }
+    });
 
-    titlebar.addEventListener("touchmove", function(e) {
+    titlebar.on("touchmove", function(e) {
         move(e.pageX, e.pageY);
     });
     return win;
@@ -38,10 +38,10 @@ Winter.App = function(name, about) {
     this.content = Winter.CreateWindow(name);
 }
 Winter.App.prototype.setWidth = function(w) {
-    this.content.style.minWidth = w + "px";
+    this.content.css("minWidth", w + "px");
 }
 Winter.App.prototype.setHeight = function(h) {
-    this.content.style.minHeight = h + "px";
+    this.content.css("minHeight", h + "px");
 }
 Winter.App.prototype.close = function() {
     this.content.remove();
@@ -49,45 +49,45 @@ Winter.App.prototype.close = function() {
 
 Winter.WebApp = function(name, about, url) {
     Winter.App.call(this, name, about);
-    this.frame = document.createElement("iframe");
-    this.frame.style.border = 0;
-    this.content.appendChild(this.frame);
-    this.frame.src = url;
-    this.frame.width = 200;
-    this.frame.height = 200;
+    this.frame = $("<iframe/>");
+    this.frame.css("border", 0);
+    this.frame.appendTo(this.content);
+    this.frame.attr("src", url);
+    this.frame.attr("width", 200);
+    this.frame.attr("height", 200);
 }
 Winter.WebApp.prototype = Object.create(Winter.App.prototype);
 Winter.WebApp.prototype.setWidth = function(w) {
     Winter.App.prototype.setWidth.call(this, w);
-    this.frame.width = w;
+    this.frame.attr("width", w);
 }
 Winter.WebApp.prototype.setHeight = function(h) {
     Winter.App.prototype.setHeight.call(this, h);
-    this.frame.width = h;
+    this.frame.attr("height", h);
 }
 
 Winter.Apps = {};
 Winter.Apps.Browser = function(url) {
     this.app = new Winter.App("Browser", "Browse the web");
-    this.address = document.createElement("input");
-    this.address.value = "https://start.duckduckgo.com/";
-    this.address.style.display = "block";
-    this.address.style.width = "100%";
-    this.address.style.padding = "8px";
-    this.address.style.boxSizing = "border-box";
+    this.address = $("<input type=\"text\"/>");
+    this.address.val("https://start.duckduckgo.com/");
+    this.address.css("display", "block");
+    this.address.css("width", "100%");
+    this.address.css("padding", "8px");
+    this.address.css("boxSizing", "border-box");
     var self = this;
-    this.address.onkeypress = function(e) {
+    this.address.keypress(function(e) {
         if(e.keyCode == 13)
             self.goto(this.value);
-    }
-    this.address.style.border = 0;
-    this.app.content.appendChild(this.address);
-    this.frame = document.createElement("iframe");
-    this.frame.style.border = 0;
-    this.app.content.appendChild(this.frame);
-    this.frame.src = "https://start.duckduckgo.com/";
-    this.frame.width = "500px";
-    this.frame.height = "500px";
+    });
+    this.address.css("border", 0);
+    this.address.appendTo(this.app.content);
+    this.frame = $("<iframe/>");
+    this.frame.css("border", "0");
+    this.frame.appendTo(this.app.content);
+    this.frame.attr("src", "https://start.duckduckgo.com/");
+    this.frame.attr("width", "500px");
+    this.frame.attr("height", "500px");
     this.app.setWidth(500);
     this.app.setHeight(500);
 }
@@ -104,11 +104,11 @@ Winter.Apps[2048] = function() {
     this.app.setWidth(300);
 }
 
-window.addEventListener("load", function() {
-    document.body.addEventListener("mousedown", function() { 
+$(document).ready(function() {
+    $(document.body).mousedown(function() { 
         Winter.mousedown = 1;
     });
-    document.body.addEventListener("mouseup", function() {
+    $(document.body).mouseup(function() {
         Winter.mousedown = 0;
     });
 });
